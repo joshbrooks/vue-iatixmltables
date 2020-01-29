1a3b7f44f6db6239af1cdd54f6a6a8d1067f2da6
Joshua Brooks <josh.vdbroek@gmail.com>
2020-01-29 15:07:49 +0900

    vetur installed

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ npm i -g @vue/cli
 vue create iatixmltables
 
 vue add vuetify
+
+# Running 
+
+You can do this as a more "permanent" solution
+
+(npm install -g serve)
+
+yarn run build
+cd dist
+serve .
+
+
+yarn run serve
--- iatixmltables/src/components/Welcome.vue
+++ iatixmltables/src/components/Welcome.vue
@@ -1,13 +1,48 @@
 <template>
   <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
+    <v-layout text-center wrap fluid>
+      <v-app-bar
+        :clipped-left="$vuetify.breakpoint.lgAndUp"
+        app
+        color="blue darken-3"
+        dark
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+        <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+          <span class="hidden-sm-and-down">AIMS Myanmar</span>
+        </v-toolbar-title>
+        <v-text-field
+          flat
+          solo-inverted
+          hide-details
+          prepend-inner-icon="mdi-magnify"
+          label="Search"
+          class="hidden-sm-and-down"
+        />
+        <v-spacer />
+        <v-btn icon>
+          <v-icon>mdi-apps</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-bell</v-icon>
+        </v-btn>
+        <v-btn icon large>
+          <v-avatar size="32px" item>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+              alt="Vuetify"
+          /></v-avatar>
+        </v-btn>
+      </v-app-bar>
+
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to your AIMS store
         </h1>
+        <v-alert type="success">
+          I'm a success alert.
+        </v-alert>
+        <p>{{ info }}</p>
       </v-flex>
     </v-layout>
   </v-container>
@@ -15,61 +50,17 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import axios from 'axios'
 
 export default Vue.extend({
   name: 'Welcome',
 
   data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader'
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify'
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify'
-      }
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com'
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com'
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify'
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs'
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify'
-      }
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer'
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined'
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-      }
-    ]
-  })
+    info: 'Hi'
+  }),
+
+  mounted() {
+    axios.get('http://example.com').then(response => (this.info = 'Hello! '))
+  }
 })
 </script>
