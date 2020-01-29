e83735e3cf17b3d31d41bc209e3e1f91e1f32640
Joshua Brooks <josh.vdbroek@gmail.com>
2020-01-29 16:46:18 +0900

    maps in template

--- iatixmltables/src/components/Welcome.vue
+++ iatixmltables/src/components/Welcome.vue
@@ -1,7 +1,6 @@
 <template>
   <v-container>
     <v-layout text-center wrap fluid>
-
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to your AIMS store
@@ -11,6 +10,14 @@
         </v-alert>
         <p>{{ info }}</p>
       </v-flex>
+      <v-flex mb-4>
+        <v-map :zoom="13" :center="[47.41322, -1.219482]">
+          <v-tilelayer
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          ></v-tilelayer>
+          <v-marker :lat-lng="[47.41322, -1.219482]"></v-marker>
+        </v-map>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -18,7 +25,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import axios from 'axios'
-
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
 export default Vue.extend({
   name: 'Welcome',
 
